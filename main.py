bb37bf930f6d77be8274a6bc04962fa6044d185e
Vasyl Faifura <vasyl.faifura@fortegrp.com>
2022-12-01 01:43:25 +0200

    added __init__.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ while(True):
             connectionStatus = "ON :)"
             return connectionStatus
         else:
-            connectionStatus = "<font color='red'>DOWN"
+            connectionStatus = "TEST"
             return connectionStatus
     webHook(checkConnection())
     time.sleep(300)
